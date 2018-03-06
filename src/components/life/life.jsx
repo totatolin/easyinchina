619f1b66b6f417d38ae51ec45560974629ab87c0
SennaLyn <totatolin@foxmail.com>
2018-03-06 18:39:34 +0800

    feat:设置代理

--- src/components/life/life.jsx
+++ src/components/life/life.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 // import './life.scss';
 
 class Life extends Component {
+	constructor () {
+		super();
+    axios.get('/proxy-api/test/test')
+    .then((res) => {})
+	}
   render() {
     return (
       <div className="life">
