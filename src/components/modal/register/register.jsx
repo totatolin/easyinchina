802670a8aba8d569613cdb4a6ca1250bc3ccb375
SennaLyn <totatolin@foxmail.com>
2018-03-15 20:17:08 +0800

    feat:注册页面

--- src/components/modal/register/register.jsx
+++ src/components/modal/register/register.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import qs from 'qs';
 import './register.scss';
 
 class Register extends React.Component {
+  register = () => {
+    axios.post('/proxy-api/account/register', qs.stringify({
+      username: 'linsen',
+      password: 'ls3535135'
+    }))
+    .then((res) => {})
+  }
   render() {
     return (
       <div className="register">
@@ -11,10 +20,10 @@ class Register extends React.Component {
         <div className="input">
           <input type="text" placeholder="E-mail address" className="wfull h50"></input>
           <input type="text" placeholder="Username" className="wfull h50"></input>
-          <input type="password" placeholder="Password" className="wfull h50" autocomplete="new-password"></input>
-          <input type="password" placeholder="Confirm password" className="wfull h50" autocomplete="new-password"></input>
+          <input type="password" placeholder="Password" className="wfull h50"></input>
+          <input type="password" placeholder="Confirm password" className="wfull h50"></input>
         </div>
-        <div className="btn wfull h40 ft18 lh40 mt30 color-white bc-blue1" onClick={this.login}>Register</div>
+        <div className="btn wfull h40 ft18 lh40 mt30 color-white bc-blue1" onClick={this.register}>Register</div>
       </div>
     );
   }
