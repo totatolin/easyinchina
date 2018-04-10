54834276c1575a6d2f462f330ada8386fdf58367
SennaLyn <totatolin@foxmail.com>
2018-04-11 07:50:56 +0800

    feat:头部样式修改

--- src/components/head/head.jsx
+++ src/components/head/head.jsx
@@ -2,17 +2,44 @@ import React, { Component } from 'react';
 import './head.scss';
 import Modals from '../modal/modal.jsx';
 import {Provider, connect} from 'react-redux';
+import { Menu, Dropdown, Icon } from 'antd';
 import { mapStateToProps, mapDispatchToProps } from '../../redux/action/index';
 
 class Head extends Component {
+  state = {
+    hasLoginin: true
+  }
   render() {
+    var login = null
+    const menu = (
+      <Menu>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">3rd menu item</a>
+        </Menu.Item>
+      </Menu>
+    )
+    if (this.state.hasLoginin) {
+      login = <Dropdown overlay={menu} placement="bottomCenter">
+                <a className="ant-dropdown-link" href="#">
+                  Username 
+                </a>
+              </Dropdown>
+    } else {
+      login = <Modals type="login" />
+    }
     return (
       <div className="head ft20 color-white">
       	<div className="head-content">
       		<p className="pull-left">Easy in HangZhou</p>
           <input className="w300 h40"></input>
           <div className="pull-right">
-            <Modals type="login" />
+            {login}
           </div>
       	</div>
       </div>
