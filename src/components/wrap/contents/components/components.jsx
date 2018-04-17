90e56fee0ecc97f30e6c8c7999298c9f156e9261
SennaLyn <totatolin@foxmail.com>
2018-04-17 19:18:37 +0800

    feat:布局

--- src/components/wrap/contents/components/components.jsx
+++ src/components/wrap/contents/components/components.jsx
@@ -9,6 +9,19 @@ class Item extends React.Component {
   render () {
     return (
       <div className="item">
+        <div className="wfull h200">
+          <img className="w200 h200 pull-left" src={require('../../../../images/1.jpg')} />
+          <div className="w200 h200 ml10 pull-left">
+            <p className="ft18">The name of shop</p>
+            <p>Dinner;American;Burger;(09:00~22:00)</p>
+            <p>Address:Yuhang xxRoad</p>
+            <p>地址： 余杭神马路36号</p>
+            <p>电话： +8613767768954</p>
+          </div>
+        </div>
+        <div className="wfull h50">
+          Brief: This shop is especially for people who like meat.With hundreds of ways to cook,you will enjoy it.read more
+        </div>
       </div>
     )
   }
